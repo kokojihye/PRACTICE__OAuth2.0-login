8c2bc92d91bd7826791e0b0707e76281c32a41bf
kokojihye <jihyevivianako@gmail.com>
2023-10-02 22:27:17 +0900

    Initial commit

---
+++ src/main/java/com/example/oauth2kakaologin/main/Oauth2KakaoLoginApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauth2kakaologin.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2KakaoLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2KakaoLoginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oauth2kakaologin/Oauth2KakaoLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauth2kakaologin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2KakaoLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
